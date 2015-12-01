0a76763c2b19cced3976813558f1bf86b6d63c76
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-12-01 12:01:27 +0100

    Make smaller windows full screen on live window

--- cpp/OpenCVHelpers.cpp
+++ cpp/OpenCVHelpers.cpp
@@ -1,60 +1,82 @@
 #include "OpenCVHelpers.hpp"
 
 typedef std::pair<int, int> _pT;
-#define _pV(v1, v2) std::pair<int,int>(v1, v2)  
+#define _pV(v1, v2) std::pair<int,int>(v1, v2)
 
 inline _pT _pSize(cv::Mat & m) {
-	return _pV(m.cols, m.rows);
+    return _pV(m.cols, m.rows);
 }
 
 inline _pT _pScale(_pT x, float f ) {
-	return _pV(x.first * f, x.second * f);
+    return _pV(x.first * f, x.second * f);
 }
 
 inline _pT _pSub(_pT p1, _pT p2) {
-	return _pV(p1.first - p2.first, p1.second - p2.second);
+    return _pV(p1.first - p2.first, p1.second - p2.second);
 }
 
 std::pair<cv::Rect, cv::Size> pad(cv::Mat & m, float padding) {
-	auto paddedSize = _pScale(_pSize(m), (1.0f - (padding)));
-	auto borderSize = _pSub(_pSize(m),paddedSize);
-	auto orig       = _pV(borderSize.first/2, borderSize.second/2);
-	auto rect       = cv::Rect(orig.first, orig.second, paddedSize.first, paddedSize.second);
-	auto rectSize   = cv::Size(paddedSize.first, paddedSize.second);
-	return std::pair<cv::Rect, cv::Size>(rect, rectSize);
+    auto paddedSize = _pScale(_pSize(m), (1.0f - (padding)));
+    auto borderSize = _pSub(_pSize(m),paddedSize);
+    auto orig       = _pV(borderSize.first/2, borderSize.second/2);
+    auto rect       = cv::Rect(orig.first, orig.second, paddedSize.first, paddedSize.second);
+    auto rectSize   = cv::Size(paddedSize.first, paddedSize.second);
+    return std::pair<cv::Rect, cv::Size>(rect, rectSize);
 }
 
 void resizeKeepAspectRatio(cv::Mat & in, cv::Mat & out) {
-	float wWidth = out.cols;
-	float wHeight = out.rows;
-	debugMat(out);
+    float wWidth = out.cols;
+    float wHeight = out.rows;
+    debugMat(out);
 
-	float iw = in.cols;
-	float ih = in.rows;
-	if(iw > wWidth) {
-		float ratio = wWidth/ iw;
-		iw = iw * ratio;
-		ih = ih * ratio;
-	}
-	if(ih > wHeight) {
-		float ratio = wHeight/ ih;
-		iw = iw * ratio;
-		ih = ih * ratio;
-	}
-	int iiw = (int) iw;
-	int iih = (int) ih;
-	auto deltaW = std::max(0,(out.cols - iiw) >> 1);
-	auto deltaH = std::max(0,(out.rows - iih) >> 1);
-	cv::Mat dst1;
-	cv::Mat dst2;
-	resize(in, dst1, cvSize(iw, ih), 0, 0, cv::INTER_CUBIC);
-	//debugShouldEqual(dst1.cols + deltaW * 2, out.cols);
-	//debugShouldEqual(dst1.rows + deltaH * 2, out.rows);
-	auto _hRem = out.rows - deltaH - dst1.rows;
-	auto _wRem = out.cols - deltaW - dst1.cols;
-	debugShouldEqual(out.rows, dst1.rows + deltaH + _hRem);
-	debugShouldEqual(out.cols, dst1.cols + deltaW + _wRem);
-	copyMakeBorder(dst1, out, deltaH, _hRem, deltaW, _wRem, cv::BORDER_CONSTANT, 0);
+    float iw = in.cols;
+    float ih = in.rows;
+
+    auto bigger = (iw > wWidth) || (ih > wHeight);
+
+    if(bigger) {
+        if(iw > wWidth) {
+            float ratio = wWidth/ iw;
+            iw = iw * ratio;
+            ih = ih * ratio;
+        }
+        if(ih > wHeight) {
+            float ratio = wHeight/ ih;
+            iw = iw * ratio;
+            ih = ih * ratio;
+        }
+    } else {
+        if(iw > ih) {
+            float ratio = wWidth/ iw;
+            iw = std::min(wWidth, iw * ratio);
+            ih = std::min(wHeight, ih * ratio);
+        } else {
+            float ratio = wHeight/ ih;
+            iw = std::min(wWidth, iw * ratio);
+            ih = std::min(wHeight, ih * ratio);
+        }
+    }
+    // (iw x ih) Actual image size that i) keeps AR and ii) fits into final view
+    int iiw = (int) iw;
+    int iih = (int) ih;
+
+    // (deltaW x deltaH) left-horizontal and top-vertical borders to fit final view
+    auto deltaW = std::max(0,(out.cols - iiw) >> 1);
+    auto deltaH = std::max(0,(out.rows - iih) >> 1);
+
+    cv::Mat dst1;
+    resize(in, dst1, cvSize(iw, ih), 0, 0, cv::INTER_CUBIC);
+
+    // (_wRem x _hRem) right-horizontal and bottom-vertical borders ("remainders")
+    auto _hRem = std::max(0, out.rows - deltaH - dst1.rows);
+    auto _wRem = std::max(0, out.cols - deltaW - dst1.cols);
+
+    debugShouldEqual(out.rows, dst1.rows + deltaH + _hRem);
+    debugShouldEqual(out.cols, dst1.cols + deltaW + _wRem);
+
+    // Resize image 'dst1' into 'out'.
+
+    copyMakeBorder(dst1, out, deltaH, _hRem, deltaW, _wRem, cv::BORDER_CONSTANT, 0);
 }
 
 // Helper function to put text in the center of a rectangle
@@ -63,13 +85,11 @@ void centerText(cv::Mat& im, const std::string & label, double scale)
     int fontface = cv::FONT_HERSHEY_DUPLEX;
     int thickness = 1;
     int baseline = 0;
-    
+
     auto r = cv::Rect(0, 0, im.cols, im.rows);
-    
+
     cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
     cv::Point pt(r.x + (r.width-text.width)/2, r.y + (r.height+text.height)/2);
-    
+
     cv::putText(im, label, pt, fontface, scale, CV_RGB(255,255,255), thickness, 8);
 }
-
-
--- cpp/OpenCVHelpers.hpp
+++ cpp/OpenCVHelpers.hpp
@@ -14,37 +14,38 @@
 #define debugMat(name) debugm(fmt::format("{} ({}x{})", #name, name.cols, name.rows))
 #define debugVal(name) debugm(fmt::format("temporary ({})", name))
 #define debugShouldEqual(v1,v2) { debugm(fmt::format("{} ~ {}", v1, v2)) ; assert(v1 == v2); }
+#define debugShouldGEQ(v1,v2) { printf(fmt::format("{} >= {}", v1, v2)) ; assert(v1 >= v2); }
 
 extern std::pair<cv::Rect, cv::Size> pad(cv::Mat & m, float padding);
 
 template<typename Func>
 void overlayOn(cv::Mat & out, float opacity, Func lam) {
-	cv::Mat in;
-	out.copyTo(in);
-	lam(in);
-	addWeighted(in, opacity, out, 1.0 - opacity, 0.0, out);
+  cv::Mat in;
+  out.copyTo(in);
+  lam(in);
+  addWeighted(in, opacity, out, 1.0 - opacity, 0.0, out);
 }
 
 template<typename Func1, typename Func2>
 void splitV(cv::Mat & in, float alpha, Func1 lam1, Func2 lam2) {
-	float ih = (int) in.rows;
-	int ih1 = (int) (in.rows * alpha);
-	debugMat(in);
-	auto rng1 = in.rowRange(0, ih1);
-	auto rng2 = in.rowRange(ih1, ih);
-	debugMat(rng1);
-	debugMat(rng2);
-	lam1(rng1);
-	lam2(rng2);
+  float ih = (int) in.rows;
+  int ih1 = (int) (in.rows * alpha);
+  debugMat(in);
+  auto rng1 = in.rowRange(0, ih1);
+  auto rng2 = in.rowRange(ih1, ih);
+  debugMat(rng1);
+  debugMat(rng2);
+  lam1(rng1);
+  lam2(rng2);
 }
 
 template<typename Func>
 void padCanvas(cv::Mat & m, float padding, Func lam1) {
-	auto r = pad(m, padding).first;
-	auto rcol = cv::Range(r.x, r.x + r.width);
-	auto rrow = cv::Range(r.y, r.y + r.height);
-	auto submatrix = m(rrow, rcol);
-	lam1(submatrix);
+  auto r = pad(m, padding).first;
+  auto rcol = cv::Range(r.x, r.x + r.width);
+  auto rrow = cv::Range(r.y, r.y + r.height);
+  auto submatrix = m(rrow, rcol);
+  lam1(submatrix);
 }
 
 template<typename Func1, typename Func2>
